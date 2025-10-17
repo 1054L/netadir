ba69368a28d952986a5fc2a6d8bfefd38899c6fb
root <root@PO-USR239.ecenarro.local>
2025-10-17 15:18:33 +0200

    namespace

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Entity\User;
+use App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
